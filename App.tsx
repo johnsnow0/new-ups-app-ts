b4e45413b85a9b7ec7f7137f69551d50247e42c2
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2023-03-23 07:43:11 +0200

    new code

--- App.tsx
+++ App.tsx
@@ -5,14 +5,22 @@ import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigator/RootNavigator';
 import { TailwindProvider } from 'tailwind-rn';
 import utilities from './tailwind.json';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/api/sweet-lynx',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     //@ts-ignore
     <TailwindProvider utilities={utilities}>
+      <ApolloProvider client={client}>
       <NavigationContainer>
         <RootNavigator />
       </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -7,6 +7,8 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../navigator/RootNavigator'
 import { Image, Input } from '@rneui/themed'
 import { useTailwind } from 'tailwind-rn';
+import { useQuery } from '@apollo/client';
+import { GET_CUSTOMERS } from '../graphql/queries';
 
 export type CustomerScreenNavigationProp = CompositeNavigationProp<
   BottomTabNavigationProp<TabStackParamList, 'Customers'>,
@@ -16,6 +18,7 @@ const CustomersScreen = () => {
   const tw = useTailwind();
   const navigation = useNavigation<CustomerScreenNavigationProp>();
   const [input, setInput] = useState<string>('');
+
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -29,7 +32,7 @@ const CustomersScreen = () => {
         containerStyle={tw('w-full h-64')}
         PlaceholderContent={<ActivityIndicator />} />
 
-      <Input placeholder='Search by customer' value={input} onChangeText={setInput} containerStyle={tw('bg-white pt-5 pb-0 px-10')}/>
+      <Input placeholder='Search by customer' value={input} onChangeText={setInput} containerStyle={tw('bg-white pt-5 pb-0 px-10')} />
     </ScrollView>
   )
 }
